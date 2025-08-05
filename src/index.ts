163e120923992178a28efd713423c8c0e2078398
samanthakraft <kraftsam@gmail.com>
2025-08-05 16:49:29 -0400

    bundle as module

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// src/index.ts
+import UMAP from './components/UMAP/wrapper.vue'
+// export any composables too
+export * from './composables/useGetToken'
+
+export default UMAP
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    lib: {
+      entry: './src/index.ts', 
+      name: 'PennsieveVisualization',
+      fileName: (format) => `pennsieve-visualization.${format}.js`,
+    },
+    rollupOptions: {
+      //external dependencies 
+      external: ['vue', 'aws-amplify'],
+      output: {
+        globals: {
+          vue: 'Vue',
+          'aws-amplify': 'Amplify',
+        },
+      },
+    },
+  },
 })
